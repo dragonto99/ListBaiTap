a778de45c62e7f9e22f590a2f85f047bdb9115f1
phuongvn666 <93229788+phuongvn666@users.noreply.github.com>
2023-03-01 10:14:09 +0700

    Update 1.dart

--- ListBT/Baitap5/1.dart
+++ ListBT/Baitap5/1.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main(List<String> args) {
   File file = File('hello.txt');
-  file.writeAsStringSync('Thân Ngọc Long');
+  file.writeAsStringSync('Phuong');
   print("Write success");
   
 }
